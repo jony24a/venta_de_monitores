209b4ec572fddc947d51a9a186704cdbd2d3f176
jony24a <127144327+jony24a@users.noreply.github.com>
2023-09-04 08:30:20 -0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,16 @@
 CREATE DATABASE ventas_de_monitores;
+
+/* Table [ clientes ] */
 CREATE TABLE IF NOT EXISTS clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_cliente VARCHAR(15) NOT NULL,
     cedula_cliente VARCHAR(50) NOT NULL,
     celular_cliente VARCHAR(50) NOT NULL
+);
+
+/* Table [ monitores ] */
+CREATE TABLE IF NOT EXISTS monitores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(15) NOT NULL,
+    precio INT NOT NULL
 );
